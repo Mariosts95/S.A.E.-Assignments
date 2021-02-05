fb50dda6da8db6b052d2773fcd42e5640e0fe7be
Marios Tsioutsis <72659932+Mariosts95@users.noreply.github.com>
2021-02-05 17:17:54 +0200

    added a bit of animation

--- Contact_Form/js/script.js
+++ Contact_Form/js/script.js
@@ -24,17 +24,6 @@ const phone_number_error = document.getElementById('phone-number-error');
 const address_error = document.getElementById('address-error');
 const planet_error = document.getElementById('planet-error');
 
-/* 
-console.log(fullname);
-console.log(password);
-console.log(confirm_password);
-console.log(email);
-console.log(confirm_email);
-console.log(phone_number);
-console.log(address);  
-console.log(planet);
-*/
-
 //Regex
 const fullname_regex = /^[a-zA-Z]{2,}[ ][a-zA-Z]{2,}[ ]?[a-zA-Z]{2,}?$/;
 // Email regex 'stolen from the internet ^_^'
@@ -57,20 +46,35 @@ const password_weak_regex = /(?=.{8,}).*/;
 const password_medium_regex = /^(?=.{10,})(((?=.*[A-Z])(?=.*[a-z]))|((?=.*[A-Z])(?=.*[0-9]))|((?=.*[a-z])(?=.*[0-9]))).*$/;
 //Strong – If the length is 14 characters or more and has a combination of symbols, caps, text.
 const password_strong_regex = /^(?=.{16,})(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*\\W).*(?=[%@!$&?])$/;
+
+//Obi-Wan Kenobi gif
+function helloThere() {
+    document.getElementById("hello-there").classList.add('hello-there');
+    document.getElementById("hello-there").classList.remove('hidden');
+}
+
+function byebyeThere() {
+    document.getElementById("hello-there").classList.remove('hello-there');
+    document.getElementById("hello-there").classList.add('hidden');
+}
+
+
 //Fullname check
 
 fullname.addEventListener('blur', () => {
     if (fullname.value.trim().length === 0) {
         fullname.classList.add('input-error');
-        fullname_error.innerHTML = 'So you have no name?';
+        fullname_error.innerHTML = 'So no name, you have, hmm?';
         isValid = false;
     } else if (!fullname_regex.test(fullname.value)) {
-        fullname_error.innerHTML = 'Fullname is not Valid';
-        fullname.classList.add("input-error");
+        fullname_error.innerHTML = 'Thats a name not.  Yes, hmmm.';
+        fullname.classList.add('input-error');
         isValid = false;
     } else {
         fullname_error.innerHTML = '';
-        fullname.classList.remove("input-error");
+        fullname.classList.remove('input-error');
+        helloThere();
+        setTimeout(byebyeThere, 2.4 * 1000);
         isValid = true;
     }
 });
@@ -79,16 +83,16 @@ fullname.addEventListener('blur', () => {
 
 email.addEventListener('blur', () => {
     if (email.value.trim().length === 0) {
-        email_error.innerHTML ='You dont\' have an email either?';
-        email.classList.add("input-error");
+        email_error.innerHTML = 'An email either you have not, hmm?';
+        email.classList.add('input-error');
         isValid = false;
     } else if (!email_regex.test(email.value)) {
-        email_error.innerHTML ='Are you sure this is the right email?';
-        email.classList.add("input-error");
+        email_error.innerHTML = 'Sure this is the right email are you?';
+        email.classList.add('input-error');
         isValid = false;
     } else {
         email_error.innerHTML ='';
-        email.classList.remove("input-error");
+        email.classList.remove('input-error');
         isValid = true;
     }
 });
@@ -97,16 +101,16 @@ email.addEventListener('blur', () => {
 
 confirm_email.addEventListener('blur', () => {
     if (confirm_email.value.trim().length === 0) {
-        confirm_email_error.innerHTML = 'You need to type your e-mail again';
-        confirm_email.classList.add("input-error");
+        confirm_email_error.innerHTML = 'To type your e-mail again you need.';
+        confirm_email.classList.add('input-error');
         isValid = false;
     } else if (confirm_email.value !== email.value) {
-        confirm_email_error.innerHTML = 'The e-mails need to be same';
-        confirm_email.classList.add("input-error");
+        confirm_email_error.innerHTML = 'To be same the e-mails need.';
+        confirm_email.classList.add('input-error');
         isValid = false;
     } else {
         confirm_email_error.innerHTML = '';
-        confirm_email.classList.remove("input-error");
+        confirm_email.classList.remove('input-error');
         isValid = true;
     }
 });
@@ -114,38 +118,54 @@ confirm_email.addEventListener('blur', () => {
 //Password check
 password.addEventListener('input', () => {
     if (password.value.trim().length === 0) {
-        password_error.innerHTML = 'Password is missing';
-        password.classList.add("input-error");
+        password_error.innerHTML = 'Jedi needs a password.';
+        password_error.classList.remove('password-medium');
+        password_error.classList.remove('password-strong');
+        password.classList.add('input-error');
         isValid = false;
     } else if (!password_weak_regex.test(password.value)) {
-        password_error.innerHTML = 'The Force is weak with this password';
-        password.classList.add("input-error");
+        password_error.innerHTML = 'Weak with this password, the force is.';
+        password.classList.add('input-error');
+        password_error.classList.remove('password-medium');
+        password_error.classList.remove('password-strong');
         isValid = false;
     } else if (!password_medium_regex.test(password.value)) {
-        password_error.innerHTML = 'The Force is medium with this password';
-        password.classList.remove("input-error");
+        password_error.innerHTML = 'Jedi, use a stronger password, could.';
+        password.classList.remove('input-error');
+        password_error.classList.remove('password-strong');
+        password_error.classList.add('password-medium');
         isValid = true;
     } else if (!password_strong_regex.test(password.value)) {
-        password_error.innerHTML = 'The Force is strong with this password';
-        password.classList.remove("input-error");
+        password_error.innerHTML = 'Strong with this password, the force is.  Herh herh herh.';
+        password.classList.remove('input-error');
+        password_error.classList.add('password-strong');
+        password_error.classList.remove('password-medium');
         isValid = true;
     }
 })
+
+password.addEventListener('blur', () => {
+    if(isValid){
+        password_error.innerHTML = '';
+        password_error.classList.remove('password-strong');
+        password_error.classList.remove('password-medium');
+    }
+});
 
 //Confirm Password check
 
 confirm_password.addEventListener('input', () => {
     if (confirm_password.value.trim().length === 0) {
-        confirm_password_error.innerHTML = 'You need to confirm your password';
-        confirm_password.classList.add("input-error");
+        confirm_password_error.innerHTML = 'To confirm your password you need.';
+        confirm_password.classList.add('input-error');
         isValid = false;
     } else if (confirm_password.value !== password.value) {
-        confirm_password_error.innerHTML = 'Passwords do not much'
-        confirm_password.classList.add("input-error");
+        confirm_password_error.innerHTML = 'Much passwords not.';
+        confirm_password.classList.add('input-error');
         isValid = false;
     } else {
         confirm_password_error.innerHTML = '';
-        confirm_password.classList.remove("input-error");
+        confirm_password.classList.remove('input-error');
         isValid = true;
     }
 })
@@ -154,16 +174,16 @@ confirm_password.addEventListener('input', () => {
 
 phone_number.addEventListener('blur', () => {
     if (phone_number.value.trim().length === 0) {
-        phone_number_error.innerHTML = 'Your phone number is missing';
-        phone_number.classList.add("input-error");
+        phone_number_error.innerHTML = 'We contact you, how can, hmm?';
+        phone_number.classList.add('input-error');
         isValid = false;
     } else if (!phone_number_regex.test(phone_number.value)){
-        phone_number_error.innerHTML = 'Your phone number is not valid'
-        phone_number.classList.add("input-error");
+        phone_number_error.innerHTML = 'Your phone number is valid not.';
+        phone_number.classList.add('input-error');
         isValid = false;
     } else {
         phone_number_error.innerHTML = '';
-        phone_number.classList.remove("input-error");
+        phone_number.classList.remove('input-error');
         isValid = true
     }
 });
@@ -172,16 +192,16 @@ phone_number.addEventListener('blur', () => {
 
 address.addEventListener('blur', () => {
 if (address.value.trim().length === 0) {
-    address_error.innerHTML = 'Your address is missing';
-    address.classList.add("input-error");
+    address_error.innerHTML = 'No home, you have?';
+    address.classList.add('input-error');
     isValid = false;
 } else if (!address_regex.test(address.value)) {
-    address_error.innerHTML = 'Your address is not valid';
-    address.classList.add("input-error");
+    address_error.innerHTML = 'To use a valid address you need.  Herh herh herh.';
+    address.classList.add('input-error');
     isValid = false;
 } else {
     address_error.innerHTML = '';
-    address.classList.remove("input-error");
+    address.classList.remove('input-error');
     isValid = true;
 }
 });
@@ -190,16 +210,16 @@ if (address.value.trim().length === 0) {
 
 planet.addEventListener('blur', () => {
     if (planet.value.trim().length === 0) {
-    planet_error.innerHTML = 'Your planet is missing';
-    planet.classList.add("input-error");
+    planet_error.innerHTML = 'To know  your planet we need.';
+    planet.classList.add('input-error');
     isValid = false;
 } else if (!address_regex.test(planet.value)) {
-    planet_error.innerHTML = 'Your planet is not valid';
-    planet.classList.add("input-error");
+    planet_error.innerHTML = 'Of this planet never heard.';
+    planet.classList.add('input-error');
     isValid = false;
 } else {
-    planet_error.innerHTML = 'Your planet is not valid';
-    planet.classList.remove("input-error");
+    planet_error.innerHTML = '';
+    planet.classList.remove('input-error');
     isValid = true;
   }
 });
